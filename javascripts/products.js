4bbba73cb50e7b74e07d173c8822f8046d0b5999
Amay-W <amay0028@gmail.com>
2024-12-03 14:56:00 +0400

    Implemented Checkout page (checkout.html)+ Added styling in html file

--- javascripts/products.js
+++ javascripts/products.js
@@ -1,11 +1,11 @@
-let classes = [
+let products = [
     {
         id: 2001,
         title: "Public Speaking Class",
         description: "An engaging <em>interactive</em> class to build confidence and communication skills.",
         price: 2500,
         image: "assets/Pub1.jpg",
-        availableSeats: 20, // Seats available for enrollment
+        availableStock: 20, // Seats available for enrollment
         rating: 4,
     },
     {
@@ -14,7 +14,7 @@ let classes = [
         description: "Explore creativity through <em>hands-on</em> art projects and design fundamentals.",
         price: 3000,
         image: "assets/Art.png",
-        availableSeats: 15, // Seats available for enrollment
+        availableStock: 15, // Seats available for enrollment
         rating: 5,
     },
     {
@@ -23,7 +23,7 @@ let classes = [
         description: "Develop debating skills and participate in <em>Model United Nations</em> sessions.",
         price: 3500,
         image: "assets/MUN.png",
-        availableSeats: 25, // Seats available for enrollment
+        availableStock: 25, // Seats available for enrollment
         rating: 4,
     },
     {
@@ -32,7 +32,7 @@ let classes = [
         description: "A journey into the world of <em>poetry</em> with opportunities to write and share.",
         price: 1500,
         image: "assets/Poetry2.jpg",
-        availableSeats: 10, // Seats available for enrollment
+        availableStock: 10, // Seats available for enrollment
         rating: 5,
     },
     {
@@ -41,7 +41,7 @@ let classes = [
         description: "Learn the art of <em>capturing moments</em> with guidance from photography experts.",
         price: 2800,
         image: "assets/Photo.png",
-        availableSeats: 12, // Seats available for enrollment
+        availableStock: 12, // Seats available for enrollment
         rating: 4,
     },
     {
@@ -50,7 +50,7 @@ let classes = [
         description: "Explore music theory and practice with various <em>instruments</em> and styles.",
         price: 4000,
         image: "assets/Music.jpg",
-        availableSeats: 8, // Seats available for enrollment
+        availableStock: 8, // Seats available for enrollment
         rating: 5,
     },
     {
@@ -59,7 +59,7 @@ let classes = [
         description: "Master strategic thinking in our <em>chess</em> sessions for beginners and experts.",
         price: 1000,
         image: "assets/Chess.jpg",
-        availableSeats: 18, // Seats available for enrollment
+        availableStock: 18, // Seats available for enrollment
         rating: 4,
     },
 ];
